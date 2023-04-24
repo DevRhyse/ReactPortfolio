7a4f7e3495a9e93b0456181a97009d0305695fd7
Rhyse <dev.rhyse@gmail.com>
2023-04-24 17:50:46 -0500

    skill update

--- src/info/Info.js
+++ src/info/Info.js
@@ -54,8 +54,8 @@ export const info = {
     bio: "Hey there, I'm Rhyse - a Full-Stack Software Engineer who's all about growth, innovation, and having fun along the way! I'm passionate about using my problem-solving skills to deliver top-notch projects that make a real difference. As an avid learner, I'm always on the lookout for new challenges and opportunities to expand my skills. I thrive on finding creative solutions to complex problems and working in a collaborative team environment. I'm confident that I can bring a positive energy to any organization I work with, and I'm currently on the lookout for exciting new projects, clients, or employment opportunities",
     skills:
         {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'restapi', 'OOP', 'scrum', 'Agile', 'vite', 'bootstrap','tailwind','nodejs', 'express.js', 'mongodb', 'html', 'css'],
-            exposedTo: ['typescript', 'python', 'Nextjs','AWS','PostgresSql', 'c++', 'sass', 'linux', 'figma']
+            proficientWith: ['javascript', 'react', 'node', 'git', 'github', 'restapi', 'OOP', 'scrum', 'Agile', 'vite', 'bootstrap','tailwind','nodejs', 'express.js', 'mongodb', 'html', 'css'],
+            exposedTo: ['typescript', 'c#', 'docker', 'vue', 'python', 'nextjs','AWS','postgresSql', 'c++', 'sass', 'linux', 'figma']
         }
     ,
     hobbies: [
